c341e1106f3543017fd8a35fcf711cb3a1e4d4e4
Ubuntu <ubuntu@ip-10-0-1-252.me-south-1.compute.internal>
2020-10-23 15:53:38 +0000

    fix login test

--- e2e-js-test/login_test.js
+++ e2e-js-test/login_test.js
@@ -21,7 +21,7 @@
 
     casper.then(function() {
       this.click("#login-modal form button.btn.btn-primary");
-      this.waitForText("Logged in as Eve Berger", function() {
+      this.waitForText("Logged in as User Name", function() {
         test.pass("user is logged in");
       }, function() {
         test.fail("user login failed");
