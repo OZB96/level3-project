552d0741928428d733241c38400c5114a4ae8a5b
Ubuntu <ubuntu@ip-10-0-1-252.me-south-1.compute.internal>
2020-10-23 16:33:32 +0000

    add click event

--- e2e-js-test/checkout_test.js
+++ e2e-js-test/checkout_test.js
@@ -65,13 +65,14 @@
 
     // actually checkout
     casper.then(function() {
+      this.clickLabel("Proceed to checkout");
       this.waitForText("My orders", function() {
         test.pass("user is taken to the orders page");
       }, function() {
-        console.log("dumping page screenshot as PNG")
-        var cap = casper.captureBase64("png");
+        //console.log("dumping page screenshot as PNG")
+        //var cap = casper.captureBase64("png");
         //console.log(cap);
-        console.log("DONE");
+        //console.log("DONE");
         test.fail("user was not taken to the orders page");
       }, 3000);
     });
