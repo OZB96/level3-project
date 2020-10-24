d4eaaf239a60cd98fec64c3eb329cf8c4126aa5d
Ubuntu <ubuntu@ip-10-0-1-208.me-south-1.compute.internal>
2020-10-24 04:28:01 +0000

    get back

--- e2e-js-test/checkout_test.js
+++ e2e-js-test/checkout_test.js
@@ -4,7 +4,7 @@
 
   var __utils__ = require("clientutils").create();
 
-  casper.test.begin("User buys some socks", 1, function(test) {
+  casper.test.begin("User buys some socks", 5, function(test) {
     // initial load and login
     casper.start("http://front-end:8080/", function() {
       this.clickLabel("Login");
