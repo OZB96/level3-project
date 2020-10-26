4e5c686f04bbd49a834a58e4c32fac9450c6b7e6
Ubuntu <ubuntu@ip-10-0-1-180.me-south-1.compute.internal>
2020-10-26 19:31:29 +0000

    add runner

--- run.sh
+++ run.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+i=1
+numberOfService=13
 kubectl get pods -n test | grep -oe '\([0-9.]\/[0-9.]\)' | while read -r line ; do
-if [ $line = "1/1" || $line = "2/2"]
+i=$((++i))
+if [ $line = "1/1" ] || [ $line = "2/2" ]
 then
-	echo correct.
-	pwd
+echo "service $(kubectl get pods -n test | sed -n ${i}p | cut -d ' ' -f 1) is running" 2> /dev/null 
+numberOfService=$((--numberOfService))
+fi
+if [ $numberOfService -eq 0 ] 
+then
+echo "done"
+break
 fi
 done
+
