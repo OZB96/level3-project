434208a2ce22cb04ab0ed4e0edd643571bb9486a
Ubuntu <ubuntu@ip-10-0-1-137.me-south-1.compute.internal>
2020-10-28 18:12:05 +0000

    fix the tests

--- e2e-js-test/checkout_test.js
+++ e2e-js-test/checkout_test.js
@@ -6,7 +6,7 @@
 
   casper.test.begin("User buys some socks", 7, function(test) {
     // initial load and login
-    casper.start("http://front-end:8080/", function() {
+    casper.start("http://front-end/", function() {
       test.assertNotVisible("#login-modal", "user does not see the login dialogue");
       this.clickLabel("Login");
       casper.waitUntilVisible("#login-modal", function() {
